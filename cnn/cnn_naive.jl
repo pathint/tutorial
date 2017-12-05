ac43f598d7fb5b525dc051cb8aa28e89c5bd854a
Xianlong Wang <wang.xianlong@139.com>
2017-12-05 13:52:46 +0800

    Add files via upload

--- cnn/cnn_naive.jl
+++ cnn/cnn_naive.jl
@@ -3,9 +3,10 @@ pwd()
 ENV["MOCHA_USE_NATIVE_EXT"] = "true"
 ENV["OMP_NUM_THREADS"] = 4
 using Mocha
+
 data_layer  = HDF5DataLayer(name="train-data", 
                             source="train.txt",
-                            batch_size=64, 
+                            batch_size=100, 
                             shuffle=true)
 conv_layer = ConvolutionLayer(name="conv1", 
                               n_filter=20, 
@@ -24,20 +25,17 @@ pool2_layer = PoolingLayer(name="pool2",
                            stride=(4,1),
                            bottoms=[:conv2], tops=[:pool2])
 fc1_layer  = InnerProductLayer(name="ip1", 
-                               output_dim=500,
-                               neuron=Neurons.ReLU(), 
+                               output_dim=50,
+                               neuron=Neurons.Tanh(), 
                                bottoms=[:pool2], tops=[:ip1])
 fc2_layer  = InnerProductLayer(name="ip2", 
-                               output_dim=10,
-                               bottoms=[:ip1], tops=[:ip2])
-fc3_layer  = InnerProductLayer(name="ip3", 
                                output_dim=1,
-                               bottoms=[:ip2], tops=[:ip3])
+                               bottoms=[:ip1], tops=[:ip2])
 loss_layer = SquareLossLayer(name="loss", 
-                              bottoms=[:ip3,:label])
+                              bottoms=[:ip2,:label])
 common_layers = [conv_layer, pool_layer, 
                  conv2_layer, pool2_layer,
-                 fc1_layer, fc2_layer, fc3_layer]
+                 fc1_layer, fc2_layer]
 
 backend = CPUBackend()
 init(backend)
@@ -54,34 +52,40 @@ params = make_solver_parameters(method,
                                 lr_policy=LRPolicy.Inv(0.01, 0.0001, 0.75),
                                 load_from=exp_dir)
 solver = Solver(method, params)
-
+# every 1000 iterations, save the statistics to disk
 setup_coffee_lounge(solver, 
                     save_into="$exp_dir/statistics.jld", 
                     every_n_iter=1000)
-
+# every 100 iterations, show the training summary
 add_coffee_break(solver, 
                  TrainingSummary(), 
-                 every_n_iter=100)
-
+                 every_n_iter=50)
+# every 500 iterations, save the snapshot
 add_coffee_break(solver, 
                  Snapshot(exp_dir), 
-                 every_n_iter=500)
-
+                 every_n_iter=5000)
+# Performace test network 
+# test data
 data_layer_test = HDF5DataLayer(name="test-data", 
                                 source="test.txt", 
-                                batch_size=64)
-acc_layer = AccuracyLayer(name="test-accuracy", 
-                          bottoms=[:ip3, :label])
+                                batch_size=100)
+acc_layer = SquareLossLayer(name="test-accuracy", 
+                        bottoms=[:ip2, :label])
 test_net = Net("MNIST-test", 
                backend, 
                [data_layer_test, common_layers..., acc_layer])
-
+# every 500 iterations, performance validation on test data
 add_coffee_break(solver, 
                  ValidationPerformance(test_net), 
                  every_n_iter=500)
 
+# let's solve it!
 solve(solver, net)
 
+dump_statistics(solver.coffee_lounge. get_layer_state(net, "loss", true))
 
-
+# free memory
+destroy(net)
+destroy(test_net)
+shutdown(backend)
 
