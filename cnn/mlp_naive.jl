2372bf42442b72cbfd9f9599f14a383e86991adc
Xianlong Wang <wang.xianlong@139.com>
2017-12-05 16:51:18 +0800

    Add files via upload

--- cnn/mlp_naive.jl
+++ cnn/mlp_naive.jl
@@ -7,34 +7,20 @@ data_layer  = HDF5DataLayer(name="train-data",
                             source="train.txt",
                             batch_size=100, 
                             shuffle=true)
-conv_layer = ConvolutionLayer(name="conv1", 
-                              n_filter=20, 
-                              kernel=(25,1),
-                              bottoms=[:data], tops=[:conv1])
-pool_layer = PoolingLayer(name="pool1", 
-                          kernel=(4,1), 
-                          stride=(4,1),
-                          bottoms=[:conv1], tops=[:pool1])
-conv2_layer = ConvolutionLayer(name="conv2", 
-                               n_filter=50, 
-                               kernel=(25,1),
-                               bottoms=[:pool1], tops=[:conv2])
-pool2_layer = PoolingLayer(name="pool2", 
-                           kernel=(4,1), 
-                           stride=(4,1),
-                           bottoms=[:conv2], tops=[:pool2])
+fc0_layer  = InnerProductLayer(name="ip0", 
+                               output_dim=500,
+                               neuron=Neurons.Tanh(), 
+                               bottoms=[:data], tops=[:ip0])
 fc1_layer  = InnerProductLayer(name="ip1", 
                                output_dim=50,
                                neuron=Neurons.Sigmoid(), 
-                               bottoms=[:pool2], tops=[:ip1])
+                               bottoms=[:ip0], tops=[:ip1])
 fc2_layer  = InnerProductLayer(name="ip2", 
                                output_dim=1,
                                bottoms=[:ip1], tops=[:ip2])
 loss_layer = SquareLossLayer(name="loss", 
                               bottoms=[:ip2,:label])
-common_layers = [conv_layer, pool_layer, 
-                 conv2_layer, pool2_layer,
-                 fc1_layer, fc2_layer]
+common_layers = [fc0_layer, fc1_layer, fc2_layer]
 
 backend = CPUBackend()
 init(backend)
@@ -42,10 +28,10 @@ net = Net("merck-train",
           backend, 
           [data_layer, common_layers..., loss_layer])
 
-exp_dir = "snapshots"
+exp_dir = "mlp_snapshots"
 method = SGD()
 params = make_solver_parameters(method, 
-                                max_iter=2000, 
+                                max_iter=10000, 
                                 regu_coef=0.0005,
                                 mom_policy=MomPolicy.Fixed(0.9),
                                 lr_policy=LRPolicy.Inv(0.01, 0.0001, 0.75),
@@ -54,15 +40,15 @@ solver = Solver(method, params)
 # every 1000 iterations, save the statistics to disk
 setup_coffee_lounge(solver, 
                     save_into="$exp_dir/statistics.jld", 
-                    every_n_iter=500)
+                    every_n_iter=1000)
 # every 100 iterations, show the training summary
 add_coffee_break(solver, 
                  TrainingSummary(), 
-                 every_n_iter=50)
+                 every_n_iter=100)
 # every 500 iterations, save the snapshot
 add_coffee_break(solver, 
                  Snapshot(exp_dir), 
-                 every_n_iter=500)
+                 every_n_iter=2000)
 # Performace test network 
 # test data
 data_layer_test = HDF5DataLayer(name="test-data", 
